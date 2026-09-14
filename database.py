f17114fa0d3c987ec1470d83a52b038bda9d1694
unknown <blueuk@naver.com>
2026-09-14 21:14:23 +0900

    Force load_dotenv to override OS env vars

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import declarative_base
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
